dbc20e0b021532acfa7b0dd49dac772f8d378c8a
DESKTOP-K39UOR0\Jaden <jadenpalmerleandre@hotmail.co.uk>
2017-02-07 14:30:51 +0000

    Engine changed to 4.14. This is now the new engine version I am working with.

---
+++ BuildingEscape 4.14 - 6/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,36 @@
+// Copyright Jaden Palmer-Leandre 2016
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void OpenDoor();
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
+
+	UPROPERTY(VisibleAnywhere)
+		float OpenAngle = 90.0f;
+
+
+		AActor* ActorThatOpens; // pawn inherits from actor
+	
+};
